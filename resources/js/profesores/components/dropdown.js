c09496da0e6afbc3e2314a4b00f12a6f2dc0f2bc
aaronPro <aaroncascino@gmail.com>
2025-07-07 00:05:20 -0300

    Arreglos y asistencias

---
+++ resources/js/profesores/components/dropdown.js
@@ -0,0 +1,6 @@
+const dropdowns = $(".asistencia-dropdown");
+
+dropdowns.each(function (index, dropdown) {
+    const button = dropdown.dataset("id");
+    const currentDropdown = dropdown.dataset("id");
+});
