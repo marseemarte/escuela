6bffb0096d5f563edc4c61366ab1190c8234f22c
RUYKx <aaroncascino@gmail.com>
2025-08-08 21:33:57 -0300

    adaptar diseno de botones

--- resources/views/partials/profesores/asistencias/asistencias-tomar.blade.php
+++ resources/views/partials/profesores/asistencias/asistencias-tomar.blade.php
@@ -4,9 +4,9 @@
 
 <x-profesores.section-container>
     <div class="py-3.5 px-5 grid grid-cols-1 md:grid-cols-3 lg:grid-cols-6 gap-4">
-        <input type="text"
-            class="w-full px-4 py-2 text-sm border border-gray-300 rounded-lg bg-gray-100 focus:ring-blue-500 focus:border-blue-500"
-            placeholder="Buscar Alumno…">
+
+        <x-profesores.input.text style="search" placeholder="Buscar Alumno" />
+
         <x-profesores.dropdown.button id="0" defaultLabel="Seleccionar">
             <x-profesores.dropdown.menu content="Presente" />
             <x-profesores.dropdown.menu content="Ausente" />
