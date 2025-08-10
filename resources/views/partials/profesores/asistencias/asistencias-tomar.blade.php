e6319882f291934ba771ef10b49802f268e97ea4
aaronPro <aaroncascino@gmail.com>
2025-08-10 18:48:14 -0300

    busqueda por el cliente

--- resources/views/partials/profesores/asistencias/asistencias-tomar.blade.php
+++ resources/views/partials/profesores/asistencias/asistencias-tomar.blade.php
@@ -7,10 +7,11 @@
         <input type="text"
             class="w-full px-4 py-2 text-sm border border-gray-300 rounded-lg bg-gray-100 focus:ring-blue-500 focus:border-blue-500"
             placeholder="Buscar Alumno…">
-        <x-profesores.dropdown.button id="0" defaultLabel="Seleccionar">
+        <x-profesores.dropdown.button id="0" defaultLabel="Seleccionar asistencia">
             <x-profesores.dropdown.menu content="Presente" />
             <x-profesores.dropdown.menu content="Ausente" />
             <x-profesores.dropdown.menu content="Justificado" />
+            <x-profesores.dropdown.menu content="Todos" />
         </x-profesores.dropdown.button>
         <div class="flex items-center space-x-3">
             <button type="button"
