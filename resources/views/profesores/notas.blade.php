89eb9e35c5358a328e5dde6258efcebe90fadf11
Sosa Federico <federicocp4@gmail.com>
2025-08-17 16:24:50 -0300

    si

--- resources/views/profesores/notas.blade.php
+++ resources/views/profesores/notas.blade.php
@@ -1,15 +1,15 @@
 <x-layouts.profesores.dashboard notas titulo="Notas">
 
-    <form action="{{ route('profesores.notas.materias') }}" method="post">
+    <form action="{{ route('profesores.notas.materias.lista') }}" method="post">
+        <h1>Materias</h1>
         @csrf
-        <h2 class="mt-4"> Ciclo Basico</h2>
 
-        <input type="submit" value="1°G" name="curso"
+        <input type="submit" value="Ingles" name="curso"
             class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded cursor-pointer mt-4">
 
         <h2 class="mt-4">Ciclo Superior</h2>
 
-        <input type="submit" value="7°C" name="curso"
+        <input type="submit" value="Practicas Profesionalizantes" name="curso"
             class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded cursor-pointer mt-4">
     </form>
 </x-layouts.profesores.dashboard>
