8b2c265506e88861653e042bbdde11555ee02fad
aaronPro <aaroncascino@gmail.com>
2025-09-12 21:00:17 -0300

    model materias

--- app/Models/Materia.php
+++ app/Models/Materia.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Materia extends Model
 {
@@ -12,7 +13,11 @@ class Materia extends Model
         'estado',
         'resumen',
     ];
-
+    // Relaciones
+    public function cupof(): HasMany
+    {
+        return $this->hasMany(Cupof::class, 'id_materias');
+    }
     // Scopes y Accessors
     public function scopeHabilitadas($query)
     {
